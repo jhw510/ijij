bc2c73c6ec133033088da2850de06d5e234bc616
jhw510 <jhw510@hanmail.net>
2020-05-18 10:47:02 +0900

    200516 10:47

--- src/vue/src/main.js
+++ src/vue/src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import Vuex from 'vuex'
 Vue.config.productionTip = false
-
+Vue.use(Vuex)
 new Vue({
   vuetify,
   router,
